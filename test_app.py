09fa87b5c259ed22975dc925d12db9714c40589c
osamanajjarr <osamaabsabti@gmail.com>
2026-04-29 03:11:01 +0300

    correction

--- test_app.py
+++ test_app.py
@@ -1,5 +1,7 @@
 from app import add_numbers
 
 def test_addition_correct():
-    assert add_numbers(2, 3) == 5
-    
+    assert add_numbers(2, 3) == 5 # nosec B101
+
+def test_addition_fail():
+    assert add_numbers(2, 3) == 5  # nosec B101
